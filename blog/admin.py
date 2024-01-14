6d03a8c7c2213e19c0fbd26219901e6bcb1711de
Terry A. Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2024-01-14 01:31:36 -0500

    Changing Commit Authorship

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,24 @@
-from django.contrib import admin
-from blog.models import Tag, Post
-from django.utils.translation import ngettext
-from django.contrib import messages
-from django.http import HttpRequest, HttpResponse
+"""
+Module Docstring: blog.admin
+
+This module contains the admin definitions for the blog app, including the PostAdmin and TagAdmin classes.
+
+The PostAdmin class defines the admin interface for the Post model, allowing for the management and display of post data in the Django admin interface. 
+It includes custom admin actions for marking selected posts as published.
+
+The TagAdmin class defines the admin interface for the Tag model, but currently does not include any custom admin actions.
+
+To use these admin definitions, they must be registered with the Django admin site using the admin.site.register() method.
+
+"""
+
+from django.contrib import admin, messages
 from django.db.models.query import QuerySet
+from django.http import HttpRequest
+from django.utils.translation import ngettext
+
+from blog.models import Post, Tag
+
 
 # SECTION - Admin Models Defintions
 class PostAdmin(admin.ModelAdmin):
@@ -11,10 +26,24 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     list_display = ['title', 'author', 'created_at', 'published_at']
     actions = ['make_published']
-    
+
     # SECTION - Custom Admin Actions
     @admin.action(description='Mark selected posts as published')
     def make_published(self, request: HttpRequest, queryset: QuerySet[Post]) -> None:
+        """
+        Marks the selected posts as published via Admin Site.
+
+        Args:
+            self: The instance of the class.
+            request: The HTTP request object.
+            queryset: The queryset of posts to be marked as published.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
         if queryset.exists():
             updated = queryset.update(status='P')
             self.message_user(request, ngettext(
@@ -25,11 +54,11 @@ class PostAdmin(admin.ModelAdmin):
         else:
             self.message_user(request, "No posts were selected.", messages.WARNING)
 
+
 class TagAdmin(admin.ModelAdmin):
     actions = None
 
 
-    
 # SECTION - Model and Model Admin Registration 
 admin.site.register(model_or_iterable=Tag, admin_class=TagAdmin)
 admin.site.register(model_or_iterable=Post, admin_class=PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,36 +1,72 @@
-from django.db import models
+"""
+Module Docstring: models.py
+
+This module contains the definition of the Tag and Post models for the application.
+
+Tag Model:
+- The Tag model represents a tag that can be associated with a post.
+- It has a single attribute 'value' which is a TextField and serves as the primary key.
+- The model also includes a custom string representation method.
+
+Post Model:
+- The Post model represents a blog post.
+- It includes various attributes such as author, title, content, status, and tags.
+- The model also includes a custom string representation method.
+
+Both models make use of the TypedModelMeta class for meta options and include indexes and constraints for database optimization.
+"""
+
 from django.conf import settings
+from django.db import models
+from django.db.models.constraints import UniqueConstraint
+from django.db.models.functions import Lower
 from django.utils.translation import gettext_lazy as _
 from django_stubs_ext.db.models import TypedModelMeta
 
-class Tag(models.Model):
-  # Model Attrs:
-    value = models.TextField(max_length=100)
 
-  # String Representation of Tag Model
-    def __str__(self) -> str: 
-      return str(self.value)
+class Tag(models.Model):
+    # Model Attrs:
+    value = models.TextField(max_length=100, primary_key=True)
+
+    # String Representation of Tag Model
+    def __str__(self) -> str:
+        return str(self.value)
 
     class Meta(TypedModelMeta):
-      
-    
+        db_table = "tags"
+        order_with_respect_to = "value"
+        indexes = [
+            models.Index(fields=["value"], name="tag_value"),
+        ]
+        constraints = [
+      UniqueConstraint(Lower("value").desc(), name="unique_lower_name_tag")
+]
+
 class Post(models.Model):
     class STATUS(models.TextChoices):
-      DRAFT = "D", _("Draft")
-      APPROVED = "P", _("Published")
-      REJECTED = "W", _("Withdrawn")
-  # Model Attrs:
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_(""), on_delete=models.CASCADE)
+        DRAFT = "D", _("Draft")
+        APPROVED = "P", _("Published")
+        REJECTED = "W", _("Withdrawn")
+    # Model Attrs:
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Post Creator"), on_delete=models.CASCADE)
     created_at = models.DateTimeField(_("Written On"), auto_now_add=True)
     last_modified_at = models.DateTimeField(_("Last Updated On"), auto_now=True)
     published_at = models.DateTimeField(_("Posted On"), blank=True, null=True)
     title = models.CharField(_("Title"), max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     summary = models.TextField(max_length=500)
     content = models.TextField()
-    status = models.CharField(max_length=1, choices=STATUS.choices, default=STATUS.DRAFT)
+    status = models.CharField(max_length=1, choices=STATUS.choices, default=STATUS.DRAFT) 
     tags = models.ManyToManyField(Tag, related_name="posts")
 
-  # String Representation of Tag Model
-    def __str__(self) -> str: 
-      return str(self.title)
+    # String Representation of Tag Model
+    def __str__(self) -> str:
+        return str(self.title)
+
+    class Meta(TypedModelMeta):
+        db_table = "posts"
+        ordering = ["-last_modified_at"]
+        indexes = [
+            models.Index(fields=["author"], name="post_author"),
+            models.Index(fields=["status"], name="posts_status")
+        ]
